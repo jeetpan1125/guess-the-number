42f236e88cd34bcf412631bed83adbbdd52d0ef8
jeetpan1125 <jp11252006@gmail.com>
2026-07-15 12:38:38 -0400

    better flow changes to game

--- main.c
+++ main.c
@@ -10,33 +10,51 @@
 
 int main(int argc, char* argv[]) {
     
+    int play_again = 1;
     srand(time(NULL));
-    int random = rand() % 101;
-    int guess = 0;
-    int count = 1;
-    
-    printf("This is a random number guessing game. The program will generate a random integer 0-100 inclusive, and you must guess what the number is within 6 tries. After each guess the program will tell you whether if the number is less than or greater than your guess!\n\n");
-    
-    while (count <= MAX_TRIES) {
-        printf("Your guess: ");
-        scanf("%d", &guess);
+
+    while (play_again == 1) {
+
+        int random = rand() % 101;
+        int guess = 0;
+        int count = 1;
+        printf("\nThis is a random number guessing game. The program will generate a random integer 0-100 inclusive, and you must guess what the number is within 6 tries. After each guess the program will tell you whether if the number is less than or greater than your guess!\n\n");
         
-        if (guess < random) {
-            printf("Too low, try again.\n");
-            printf("Number of guesses had: %d\n\n", count);
+        while (count <= MAX_TRIES) {
+            printf("Your guess: ");
+        
+            if (scanf("%d", &guess) != 1) {
+                printf("Invalid input! Please enter a number.\n");
+                
+                while (getchar() != '\n'); // Clears the bad input from the buffer
+                continue;
+                
+            }
+            if (guess < random) {
+                printf("Too low, try again.\n");
+                printf("Number of guesses had: %d\n\n", count);
+            }
+            else if (guess > random) {
+                printf("Too high, try again.\n");
+                printf("Number of guesses had: %d\n\n", count);
+            }
+            else {
+                printf("You guess correctly! The randomly generated number was: %d\n", random);
+                break;
+            }
+            count++;
         }
-        else if (guess > random) {
-            printf("Too high, try again.\n");
-            printf("Number of guesses had: %d\n\n", count);
+        if (guess != random) {
+            printf("You ran out of tries. The randomly generated number was: %d\n", random);
         }
-        else {
-            printf("U guess correctly! The randomly generated number was: %d\n", random);
-            break;
+
+        printf ("Do you want to play again? (1 for yes, 0 for no/quit): ");
+        if (scanf("%d", &play_again) != 1) {
+            play_again = 0;
         }
-        count++;
-    }
-    if (guess != random) {
-        printf("You ran out of tries. The randomly generated number was: %d\n", random);
+        if (play_again == 0) {
+            printf("Thank ypou for playing!\n");
+        }
     }
     
     return 0;
